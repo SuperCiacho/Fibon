7a6638dc209719dea46a55ea81aa49137090c931
Bartosz Nowak <bartosznovak@gmail.com>
2017-07-01 09:59:45 +0200

    Initial commit

---
+++ Fibon.Api/Controllers/FibonacciController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fibon.Api.Controllers
+{
+    [Route("[controller]")]
+    public class FibonacciController : Controller
+    {
+        [HttpGet("{number}")]
+        public IActionResult Get(int number)
+        {
+            return this.Content("42");
+        }
+
+        [HttpPost("{number}")]
+        public IActionResult Post(int number)
+        {
+            return this.Accepted($"fibonacci/{number}", null);
+        }
+    }
+}
---
+++ Fibon.Api/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fibon.Api.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Get() => this.Content("Hello Boy");
+    }
+}
